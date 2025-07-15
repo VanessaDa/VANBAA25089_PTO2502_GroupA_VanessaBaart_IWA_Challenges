db4fdc31113441cebb96c54136594378ff26f3ec
Vanessa Gwama <vanessagwama@MacBookAir.home>
2025-07-15 09:47:05 +0200

    Add conditional shipping and warnings

--- Challenge 4/Challenge g/script.js
+++ Challenge 4/Challenge g/script.js
@@ -1,34 +1,45 @@
-FREE_WARNING = 'Free shipping only applies to single customer orders'
-BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
-NONE_SELECTED = '0'
+const FREE_WARNING = 'Free shipping only applies to single customer orders'
+const BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
+const NONE_SELECTED = '0'
+
+let currency = null
+let shipping = null
+let location = 'RSA'
+let customers = '1'
+
+
+
+const shoes = 300 * 1
+const toys = 100 * 5
+const shirts = 150 * 'NONE_SELECTED'
+const batteries = 35 * 2
+const pens = 5 * 'NONE_SELECTED' 
 
 if (location = RSA) { shipping === 400 && currency === 'R' }
 
-if location = NAM
-shipping = 600 
-else shipping = 800
-
-shoes = 300 * 1
-toys - 100 * 5
-shirts = 150 * 'NONE_SELECTED'
-batteries 35 * 2
-pens = 5 * 'NONE_SELECTED' 
-
-shipping = null
-currency = $
-
-if (shoes + batteries + pens + shirts > 1000 &&  ) {
-	if (location = NAM && customers < 2) {
-			if (location = RSA)
-		    shipping = 0 || calcShipping
-		}
-	}
+if (location == 'RSA') {
+shipping = 400 
+currency = 'R'
+}
+else 
+{
+	currency = '$'
+	shipping = location == 'NAM' ? 600 : 800
 }
 
-if (shipping = 0) && (customers !=== 1) { console.log(WARNING) }
+const isFreeLocal = currency === 'R' && total >= 1000
+const isFreeInt = currency === '$' && total >= 60
+const isFree = isFreeLocal || isFreeInt //if this is true
 
-location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
+const IS_FREE_WARNING = isFree && customers !== 1 //if this is FALSE
 
-customers = '1'
-const location = 'RSA'
-currency = null
+const totalWithShipping = total + shipping
+const isBanned = location === 'NK'
+let isChecked = ''
+if (IS_FREE_WARNING) console.log(FREE_WARNING)
+
+if (isFree && !IS_FREE_WARNING) shipping = 0
+
+console.log(isBanned ? BANNED_WARNING : 'Price: ' + currency + totalWithShipping)
+
+
